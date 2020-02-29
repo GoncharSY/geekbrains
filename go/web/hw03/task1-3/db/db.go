d5f5f9171dddb2d6bbe199678c5b3e23a1dc2d8c
gonchar.sy <gonchar.sy.dev@gmail.com>
2020-03-01 02:03:03 +0300

    Создал простую базу с данными блога

---
+++ go/web/hw03/task1-3/db/db.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+var dataPath = "C:\\Users\\serge\\Documents\\GitHub\\geekbrains\\go\\web\\hw03\\task1-3\\db\\data.json"
+var blog = Blog{}
+var errDB = fmt.Errorf("База данных не инициализинована")
+
+// Инициализировать данные.
+func init() {
+	var err error
+	var data []byte
+
+	if data, err = ioutil.ReadFile(dataPath); err != nil {
+		fmt.Println("Ошибка чтения файла данных БД: " + err.Error())
+		return
+	}
+
+	if err = json.Unmarshal(data, &blog); err != nil {
+		fmt.Println("Ошибка распаковки данных БД: " + err.Error())
+		return
+	}
+
+	errDB = nil
+	fmt.Println("База данных блога инициализирована.")
+}
+
+// GetBlog - получить блог.
+func GetBlog() (*Blog, error) {
+	return &blog, errDB
+}
+
+// Blog - описывает блог со списком постов.
+type Blog struct {
+	Name   string `json:"name"`
+	Author string `json:"author"`
+	Posts  []Post `json:"posts"`
+}
+
+// Post - описывает отдельный пост в блоге.
+type Post struct {
+	Author    string `json:"author"`
+	CreatedAt string `json:"createdAt"`
+	Text      string `json:"text"`
+	Comments  []Post `json:"comments"`
+}
