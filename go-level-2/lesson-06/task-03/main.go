592e5e64d98595d5149481138b57bc645d8bdf04
gonchar.sy <gonchar.sy.dev@gmail.com>
2022-09-10 22:35:37 +0300

    Убрал 'GOMAXPROCS' из кода

--- go-level-2/lesson-06/task-03/main.go
+++ go-level-2/lesson-06/task-03/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"sync"
 )
 
 func main() {
 	var value int
 	var wgr = sync.WaitGroup{}
-
-	runtime.GOMAXPROCS(2)
 
 	for i := 1; i <= 5; i++ {
 		wgr.Add(1)
