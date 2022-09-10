64c4f3558c82e85ebb478b11d15da3f4484970a7
gonchar.sy <gonchar.sy.dev@gmail.com>
2022-09-10 20:57:29 +0300

    Убрал бесконечный цикл и добавил сообщение в конце

--- go-level-2/lesson-06/task-02/main.go
+++ go-level-2/lesson-06/task-02/main.go
@@ -6,15 +6,17 @@ import (
 )
 
 func main() {
-	runtime.GOMAXPROCS(1)
+	// runtime.GOMAXPROCS(1)
 
 	for i := 0; i < 5; i++ {
 		go fmt.Println("Goroutine-", i)
 	}
 
-	for i := 0; ; i++ {
+	for i := 0; i < 500e6; i++ {
 		if i%1e6 == 0 {
 			runtime.Gosched()
 		}
 	}
+
+	fmt.Println("Done")
 }
